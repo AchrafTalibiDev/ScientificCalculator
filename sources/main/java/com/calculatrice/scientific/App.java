85760e3d768f3ba0a8440a893590dac834c5da6c
AchrafTalibiDev <talibi.achraf.dev@gmail.com>
2025-03-23 02:58:31 +0000

    Add files via upload

---
+++ sources/main/java/com/calculatrice/scientific/App.java
@@ -0,0 +1,10 @@
+package com.calculatrice.scientific;
+
+import com.calculatrice.scientific.ui.CalculatorUI;
+
+public class App {
+
+    public static void main(String[] args) {
+        new CalculatorUI();
+    }
+}
